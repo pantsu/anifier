8271aaef313b66c703af8af5aed2b324dfc97a82
Denis Lifanov <inadsence@gmail.com>
2012-10-13 17:25:17 +0400

    Fix broken specs

--- spec/models/release_spec.rb
+++ spec/models/release_spec.rb
@@ -18,35 +18,35 @@ describe Release do
     end
   end
 
-  describe "::create_from_parsed_data" do
+  describe "::import" do
     subject { Release }
     let(:attributes) { API::RELEASE_ATTRIBUTES.inject({}) { |hsh, attr| hsh[attr] = attr; hsh } }
 
     it "raises error if unknown attribute is passed" do
-      expect { subject.create_from_parsed_data(some_shit: 'some_shit', same_shit: 'same_shit') }\
+      expect { subject.import(bogus: 'bogus') }\
         .to raise_error(ActiveRecord::UnknownAttributeError)
     end
 
     it "creates new release" do
-      instance = subject.create_from_parsed_data(attributes)
+      instance = subject.import(attributes)
       instance.should be_persisted
     end
 
     it "sets all passed attributes" do
-      instance = subject.create_from_parsed_data(attributes)
+      instance = subject.import(attributes)
       attributes.keys.each { |attr| instance.send(attr).should == attr unless attr.in?([:title, :releaser]) }
       [:title, :releaser].each { |attr| instance.send(attr).should be_instance_of(attr.to_s.classify.constantize) }
     end
 
     %w(title releaser).each do |assoc|
       it "creates new #{assoc} is there is no needed one" do
-        expect { subject.create_from_parsed_data(attributes) }.to change(assoc.classify.constantize, :count).by(1)
+        expect { subject.import(attributes) }.to change(assoc.classify.constantize, :count).by(1)
       end
 
       it "does not create new #{assoc} if the one needed is already present" do
         klass = assoc.classify.constantize
         klass.create(name: assoc)
-        expect { subject.create_from_parsed_data(attributes) }.to_not change(klass, :count).by(1)
+        expect { subject.import(attributes) }.to_not change(klass, :count).by(1)
       end
     end
   end
