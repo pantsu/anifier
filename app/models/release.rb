729b20ed7c74dfba8d0dd4ac66332b1947f08e19
Dmitry Afanasyev <dimarzio1986@gmail.com>
2012-10-13 16:08:51 +0400

    Added Release::from_feed method

--- app/models/release.rb
+++ app/models/release.rb
@@ -9,8 +9,28 @@ class Release < ActiveRecord::Base
 
   define_index :releases do
     indexes audio, video, media
-    indexes title(:title), as: :title, sortable: true
+    indexes title(:name), as: :title, sortable: true
     set_property delta: true
     has releaser_id, created_at
   end
+
+  def self.from_feed(url)
+    API::Grabber.new(url).releases.each { |release_data| create_from_parsed_data(release_data.to_hash) }
+  end
+
+  def self.create_from_parsed_data(hash)
+    attributes = columns_hash.keys
+    create do |release|
+      hash.each do |k, v|
+        if k.to_s.in?(attributes)
+          release[k] = v
+        elsif "#{k}_id".in?(attributes)
+          release["#{k}_id"] = k.to_s.classify.constantize.first_or_create(name: v).id
+        else
+          raise ActiveRecord::UnknownAttributeError, "Unknown attribute: #{k}"
+        end
+      end
+    end
+  end
+
 end
--- spec/models/release_spec.rb
+++ spec/models/release_spec.rb
@@ -17,4 +17,33 @@ describe Release do
       it { should have_search_attribute_for(:created_at) }
     end
   end
+
+  describe "::create_from_parsed_data" do
+    subject { Release }
+    let(:attributes) { [:raw, :releaser, :title, :episodes, :volume, :extension, :audio, :video, :media, :resolution, :crc32].inject({}) { |hsh, attr| hsh[attr] = attr; hsh } }
+
+    it "raises error if unknown attribute is passed" do
+      expect { subject.create_from_parsed_data(some_shit: 'some_shit', same_shit: 'same_shit') }\
+        .to raise_error(ActiveRecord::UnknownAttributeError)
+    end
+
+    it "creates new release" do
+      instance = subject.create_from_parsed_data(attributes).should be_true
+    end
+
+    # it "sets all passed attributes" do
+    #   instance = subject.create_from_parsed_data(attributes)
+    #   attributes.keys.each { |attr| instance.send(attr).should == attr }
+    # end
+
+    %w(title releaser).each do |assoc|
+      it "creates new #{assoc} is there is no needed one" do
+
+      end
+
+      it "does not create new #{assoc} if the one needed is already present" do
+
+      end
+    end
+  end
 end
