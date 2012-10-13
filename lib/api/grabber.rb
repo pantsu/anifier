dd92a6b31ce22b08886c8ed37a732a58efa90e69
Denis Lifanov <inadsence@gmail.com>
2012-10-13 15:00:21 +0400

    simple grabber (API::Grabber)

---
+++ lib/api/grabber.rb
@@ -0,0 +1,26 @@
+class API::Grabber
+
+  def initialize(url)
+    @url = url
+    @parser = StandardParser.new
+  end
+
+  def reset!
+    @releases = nil
+  end
+
+  def releases
+    return @releases unless @releases.nil?
+    feed = Feedzirra::Feed.fetch_and_parse(@url)
+    @releases = feed.entries.map { |entry| parse_release(entry) }.compact
+  end
+
+  private
+
+  def parse_release(entry)
+    entry.title.force_encoding('UTF-8')
+    release = API::Release.build(entry.title, @parser.parse(entry.title))
+    release && release.valid? ? release : nil
+  end
+
+end
