1f6b20068c3d5a485fc770481636157fd1ed9cb1
Dmitry Afanasyev <dimarzio1986@gmail.com>
2012-10-13 21:59:50 +0400

    Removed obsolete notifications code

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,8 +2,6 @@ class Notification
   def self.deliver(user_id, release_id)
     unless Notification.exists?(release_id: release_id, user_id: user_id)
       UserMailer.delay.new_release(user, release)
-      user_id    = user.is_a?(User) ? user.id : user
-      release_id = release.is_a?(Release) ? release.id : release
       add(user_id: user_id, release_id: release_id)
     end
   end
