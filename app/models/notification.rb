764f52873657f84588c486cd35ec0366b6c3a769
Dmitry Afanasyev <dimarzio1986@gmail.com>
2012-10-13 21:58:33 +0400

    Redisified Notifications

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,10 +1,20 @@
 class Notification
-  def self.deliver(user, release)
-    unless Notification.for_user(user).for_release(release).exists?
+  def self.deliver(user_id, release_id)
+    unless Notification.exists?(release_id: release_id, user_id: user_id)
       UserMailer.delay.new_release(user, release)
       user_id    = user.is_a?(User) ? user.id : user
       release_id = release.is_a?(Release) ? release.id : release
-      create(user_id: user_id, release_id: release_id)
+      add(user_id: user_id, release_id: release_id)
     end
+  end
+
+  private
+
+  def self.exists?(args)
+    Rails.redis.sismember(args[:release_id], args[:user_id])
+  end
+
+  def self.add(args)
+    Rails.redis.sadd(args[:release_id], args[:user_id])
   end
 end
