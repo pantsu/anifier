4631cfad264547275121542e43f6f2d4515c1930
Denis Lifanov <inadsence@gmail.com>
2012-10-13 11:17:02 +0400

    API::Release

---
+++ lib/api/release.rb
@@ -0,0 +1,23 @@
+module API
+  Release = Struct.new(:raw, :releaser, :title, :episode, :extension, :audio, :video, :dimension, :crc32)
+
+  class Release
+    def self.build(raw, *elements)
+      instance = new
+      instance.raw = raw
+      instance.append *elements
+      instance
+    end
+
+    def append(*elements)
+      elements.each do |e|
+        next unless e.instance_of?(Treetop::Runtime::SyntaxNode)
+        append *e.elements unless e.elements.nil?
+
+        next unless content = (e.content rescue nil)
+        self[content.first] = content.last
+      end
+      self
+    end
+  end
+end
