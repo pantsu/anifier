ff57d5579803a09a26ad605b9930d49a306237a7
Dmitry Afanasyev <dimarzio1986@gmail.com>
2012-10-13 08:46:56 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Anyfier::Application.config.secret_token = 'cb2b036342eca2c3355c9406163ada59f32ae57d53d82e841f8cca0d00ab5aad7016eaf8d2452ab09b3644ddb5aaf32f41b5c19c6f6f3d85661f630b14b1fa9a'
