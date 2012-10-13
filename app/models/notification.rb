dd717e45bcfd7fa6f8c0c7e72c3bbc7a4acc96bb
Dmitry Afanasyev <dimarzio1986@gmail.com>
2012-10-13 22:37:14 +0400

    Hash argumentas removed from notication methods

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,18 +1,18 @@
 class Notification
   def self.deliver(user_id, release_id)
-    unless exists?(release_id: release_id, user_id: user_id)
+    unless exists?(release_id, user_id)
       UserMailer.delay.new_release(User.find(user_id), Release.find(release_id))
-      add(user_id: user_id, release_id: release_id)
+      add(release_id, user_id)
     end
   end
 
   private
 
-  def self.exists?(args)
-    Rails.redis.sismember("release-#{args[:release_id]}", args[:user_id])
+  def self.exists?(release_id, user_id)
+    Rails.redis.sismember("release-#{release_id}", user_id)
   end
 
-  def self.add(args)
-    Rails.redis.sadd("release-#{args[:release_id]}", args[:user_id])
+  def self.add(release_id, user_id)
+    Rails.redis.sadd("release-#{release_id}", user_id)
   end
 end
