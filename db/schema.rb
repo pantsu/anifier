b337e83adb7c94d6124d75cdf0f8c89a64381741
Maxim Filatov <pipopolam@gmail.com>
2012-10-13 17:26:47 +0400

    Actualized db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121013104912) do
+ActiveRecord::Schema.define(:version => 20121013111718) do
 
   create_table "delayed_jobs", :force => true do |t|
     t.integer  "priority",   :default => 0
@@ -71,7 +71,7 @@ ActiveRecord::Schema.define(:version => 20121013104912) do
   end
 
   create_table "titles", :force => true do |t|
-    t.string   "title",       :null => false
+    t.string   "name",        :null => false
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
