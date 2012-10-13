db8c83f7e56c9076336b2722e11f1ac8ea016862
Maxim Filatov <pipopolam@gmail.com>
2012-10-13 17:53:51 +0400

    User references

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+  has_many :subscriptions
+  has_many :titles, through: :subscriptions, uniq: true
+
 end
