c86d6cd9ec23db4933f1b28f6d379f7afa8f04ce
Denis Lifanov <inadsence@gmail.com>
2012-10-15 03:19:39 +0400

    custom RegistrationsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Anifier::Application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
+  devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks", registrations: "registrations" }
 
   root to: 'dashboard#index'
 
