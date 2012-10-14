9d81dc8be3b26ef904b90486719d59a1d3a86c51
Dmitry Afanasyev <dimarzio1986@gmail.com>
2012-10-14 12:21:59 +0400

    Added releasers controller specs

---
+++ spec/controllers/releasers_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe ReleasersController do
+  it "successfully renders #index action" do
+    Release.paginates_per(3)
+    3.times { create(:releaser) }
+    get :index
+    response.should be_success
+  end
+
+  it "successfully reners #show action" do
+    get :show, id: create(:releaser).to_param
+    response.should be_success
+  end
+end
