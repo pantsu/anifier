858681747d471fe59efb37bc8008e3d0357b469f
Dmitry Afanasyev <dimarzio1986@gmail.com>
2012-10-15 00:12:48 +0400

    Serach from constructor panel

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,8 +6,8 @@ class DashboardController < ApplicationController
     @releasers     = Releaser.random.limit(20)
     @subscriptions = current_user.try(:subscriptions)
     @resolutions   = Release.uniq.pluck(:resolution).compact
-    @releases = if params[:q].present?
-        Search.new(params[:q], limit: RELEASE_LIMIT).results
+    @releases = if %w(releaser_id title resolution q).any? { |p| params[p].present? }
+        Search.new(params[:title] || params[:q], releaser_id: params[:releaser_id], resolution: params[:resolution], limit: RELEASE_LIMIT).results
       else
         Release.recent.limit(RELEASE_LIMIT)
       end
